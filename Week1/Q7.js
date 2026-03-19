600d439af8a3161ab455973c68642dc6d3dc43fe
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-19 23:19:02 +0530

    Add rectangle area and perimeter calculation script

---
+++ Week1/Q7.js
@@ -0,0 +1,9 @@
+// Accept the length and width of a rectangle. Calculate & print the area and perimeter.
+let l = 12
+let w = 34
+
+const Area =(l, w)=> console.log(`Area: ${l * w}m²`);
+const Perimeter =(l, w)=> console.log(`Perimeter: ${2*(l + w)}m`);
+
+Area(l, w);
+Perimeter(l, w);    
