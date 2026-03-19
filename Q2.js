d96b0c0c340fa76072cda56d7d60220a611a3a66
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-19 10:46:59 +0530

    Done

---
+++ Q2.js
@@ -0,0 +1,9 @@
+// Print the message along with values and sum (Ex - The sum of 45 & 12 = 57)
+
+
+function sum(n1, n2){
+    let sum = n1 + n2;
+    console.log(`The sum of ${n1} & ${n2} = ${sum}`)
+}
+
+sum(12, 4)
