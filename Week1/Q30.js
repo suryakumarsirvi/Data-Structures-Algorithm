0b7e194719cebc1265ffcf171462948275a0e491
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-20 18:05:42 +0530

    Fix loop to use input number for factor calculation

--- Week1/Q30.js
+++ Week1/Q30.js
@@ -1,7 +1,7 @@
 // Print all the factors of a number.
 
 function factNum(num){
-    for(let i = 1; i<=12; i++){
+    for(let i = 1; i<=num; i++){
         if(num % i === 0){
             console.log(`${i} is Factor of ${num}`)
         }
