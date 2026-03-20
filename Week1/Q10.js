7f3445265339f3dee32b32e205afbbf667bcfb3c
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-20 08:24:21 +0530

    Add function to calculate surface area of a sphere

---
+++ Week1/Q10.js
@@ -0,0 +1,9 @@
+// Find surface area of sphere
+
+// Areaofshpere = 4πr²
+
+function Areaofshpere(radius){
+    return console.log((4 * Math.PI * Math.pow(radius, 2)).toFixed(4));
+}
+
+Areaofshpere(4)
