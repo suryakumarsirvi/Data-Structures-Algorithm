1c6e4979377b92146bb5d4b881f4e9ff87ec0b6d
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-20 17:29:38 +0530

    Add function to print "Hello World" n times

---
+++ Week1/Q25.js
@@ -0,0 +1,9 @@
+// Accept an integer and Print \Hello World\" n times"
+
+function HelloWorld(val){
+    for(let i = 1; i <= val; i++){
+        console.log('Hello World')
+    }
+}
+
+HelloWorld(15);
