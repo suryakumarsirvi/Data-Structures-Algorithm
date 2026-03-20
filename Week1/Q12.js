35a901f7c13185aa43d9dd3c6b4920a8bddbaa3c
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-20 08:33:42 +0530

    Add function to check and print the greatest of two numbers

---
+++ Week1/Q12.js
@@ -0,0 +1,11 @@
+// Accept two numbers and print the greatest between them
+
+function CheckIsGreatest(n1, n2){
+    if(n1 > n2){
+        console.log(`${n1} is greater than ${n2}`);
+    }else{
+        console.log(`${n2} is greater than ${n1}`);
+    }
+}
+
+CheckIsGreatest(17, 1.5);
