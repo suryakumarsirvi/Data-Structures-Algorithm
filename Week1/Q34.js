38e429d36a35c5cba25323f8e4cf47b2e593b27b
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-21 21:04:13 +0530

    Reverse the order of digits printed in digitSplit function

--- Week1/Q34.js
+++ Week1/Q34.js
@@ -1,7 +1,7 @@
 // Separate each digit of a number and print it on a new line (Ex - 123 -> OP: 3, 2, 1)
 
 function digitSplit(num) {
-    let splitted = num.toString().split('')
+    let splitted = num.toString().split('').reverse()
 
     for (let i = 0; i <= splitted.length - 1; i++) {
         console.log(splitted[i])
