d0f41cc1c50c26e668a5079f531218bcaf92f6e2
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-22 18:21:24 +0530

    Done

--- Week1/Q45.js
+++ Week1/Q45.js
@@ -7,21 +7,25 @@
 // Agar haan → repeat
 // Nahi → final bill print
 
+
+
 function Restaurant() {
-    let menu = [
+    let menuItems = [
         { item: "Dal Fry", price: 120 },
         { item: "Masala Paneer", price: 200 },
         { item: "Noodles", price: 150 },
         { item: "Soup", price: 100 },
         { item: "Chicken Curry", price: 250 },
         { item: "Biryani", price: 220 }
-    ];
+    ]
 
-    let totalBill = 0;
     let choice;
+    let totalBill = 0;
 
     do {
-        let order = prompt(`Menu:
+        let order;
+
+        order = prompt(`Menu:
 Dal Fry (120)
 Masala Paneer (200)
 Noodles (150)
@@ -29,41 +33,45 @@ Soup (100)
 Chicken Curry (250)
 Biryani (220)
 
-What do you want to order?`);
+What do you want to order?`)
 
         if (!order) {
-            alert("Please enter an item");
+            alert('Please enter your order to processed');
             continue;
         }
 
         order = order.trim().toLowerCase();
 
-        const foundItem = menu.find(
-            m => m.item.toLowerCase() === order
-        );
+        let isExistingItem = menuItems.find(list => list.item.toLowerCase() === order);
 
-        if (!foundItem) {
-            alert("Item not available");
+        if (!isExistingItem) {
+            alert('Not Available in MenuList.');
             continue;
         }
 
-        let qty = Number(prompt("Enter quantity:"));
+        let qty = Number(prompt('Enter how much quantity?'));
 
         if (isNaN(qty) || qty <= 0) {
             alert("Invalid quantity");
             continue;
         }
 
-        let itemTotal = foundItem.price * qty;
+        if (!qty) {
+            alert('Provide Quantity to processed');
+            continue;
+        }
+
+        let itemTotal = isExistingItem.price * qty;
         totalBill += itemTotal;
 
-        alert(`Added ₹${itemTotal} to bill`);
+        alert(`Your Item cost is ${itemTotal}`);
 
         choice = prompt("Do you want to order more? (yes/no)");
 
-    } while (choice && choice.toLowerCase() === "yes");
+    } while (choice && choice.toLowerCase() === 'yes')
 
     alert(`Your total bill is ₹${totalBill}\nThank you for ordering!`);
 }
 
-Restaurant();
+
+Restaurant()
