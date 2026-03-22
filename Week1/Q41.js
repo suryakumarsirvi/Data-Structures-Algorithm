af2efa6ef4de137558d86dad8f5cb155f02eaa65
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-22 15:54:54 +0530

    Done

---
+++ Week1/Q41.js
@@ -0,0 +1,41 @@
+// Print Weekday using Switch
+
+
+function weekiee() {
+    let day = Number(prompt('ENTER ANY WEEKDAY HERE'));
+
+    switch (day) {
+        case 1:
+            console.log('MONDAY');
+            break;
+
+        case 2:
+            console.log('TUESDAY');
+            break;
+
+        case 3:
+            console.log('WEDNESDAY');
+            break;
+
+        case 4:
+            console.log('THURSDAY');
+            break;
+
+        case 5:
+            console.log('FRIDAY');
+            break;
+
+        case 6:
+            console.log('SATURDAY');
+            break;
+
+        case 7:
+            console.log('SUNDAY');
+            break;
+
+        default:
+            console.log('Invalid Input');
+    }
+}
+
+weekiee();
