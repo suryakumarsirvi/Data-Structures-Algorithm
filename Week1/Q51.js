3e70dcec59bf16c7c0b8525c121e2a6158acd567
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-24 16:31:29 +0530

    Done

---
+++ Week1/Q51.js
@@ -0,0 +1,13 @@
+// Triangle Pattern
+
+function triangle(num) {
+    let value = ""
+
+    for (let i = 0; i < num; i++) {
+        value = " ".repeat(num - i) + "*".repeat(i * 2 + 1)
+        console.log(value)
+        value = ""
+    }
+
+}
+triangle(5)
