a27573b36b7423af68d381bb3877c90817fb192e
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-26 06:58:03 +0530

    Done

---
+++ Week2/Q55.js
@@ -0,0 +1,18 @@
+// Find the greatest element (Ex: {2, 96, 69, 77, 145, 20} = Max element = 145 found at 4 index)
+
+
+function greatest() {
+    let data = [2, 96, 69, 77, 145, 20];
+    let greaterVal = data[0];
+
+    for (let i = 1; i < data.length - 1; i++) {
+        if (data[i] >= greaterVal) {
+            greaterVal = data[i];
+        }
+    }
+
+    console.log(greaterVal);
+
+}
+
+greatest();
