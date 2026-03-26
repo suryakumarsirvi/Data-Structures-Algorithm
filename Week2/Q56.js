fc122d44db36cf29658eedf432e19fc05ae78ecd
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-03-26 11:20:12 +0530

    Done

--- Week2/Q56.js
+++ Week2/Q56.js
@@ -1,21 +1,20 @@
 // Find the second greatest element (Ex: {2, 96, 69, 77, 145, 20} = Second greatest element = 96)
 
-function secondGreatest(...data) {
-    let greatest = data[0]; 
-    let secondGreatest = greatest; 
+function secondGreatest(...data){
+    let great = data[0];
+    let secondGreat= great;
 
     for(let i = 1; i < data.length; i++){
-        if(data[i] > greatest){
-            secondGreatest = greatest; 
-            greatest = data[i];
+        if(data[i] > great){
+            secondGreat = great;
+            great = data[i];
         }
 
-        if(data[i] < greatest && secondGreatest < data[i]){
-            secondGreatest = data[i];
+        if(data[i] < great && secondGreat < data[i]){
+            secondGreat = data[i];
         }
     }
     
-    console.log(`Second Greatest = ${secondGreatest}`) 
+    console.log(secondGreat);
 }
-
 secondGreatest(2, 96, 69, 77, 145, 20, 12, 4, 191);
