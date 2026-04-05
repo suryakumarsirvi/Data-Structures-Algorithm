fb3be6cb9089a81f6c2c4eebd5f508d796308e29
suryakumarsirvi <suryakumarsirvi@gmail.com>
2026-04-05 19:03:10 +0530

    Done

--- Week2/Q85.js
+++ Week2/Q85.js
@@ -1,24 +1,23 @@
 // Count Hills and Valleys in an Array
 
 function hillsValleys(arr) {
-    let unique = [arr[0]]; // 2, 4, 1, 6, 5
+    let unique = [arr[0]];
     let count = 0;
 
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) {
+        if (arr[i] !== arr[i - 1]) {
             unique.push(arr[i])
         }
     }
 
     for (let i = 1; i < unique.length - 1; i++) {
-        if (unique[i] > unique[i - 1] && unique[i] > unique[i + 1] ||
-            unique[i] < unique[i - 1] && unique[i] < unique[i + 1]
-        ){
-            count++;
+        if (unique[i] > unique[i - 1] && unique[i] > unique[i + 1]) {
+            console.log(`Hills: ${count}`);
+        } else if (unique[i] < unique[i - 1] && unique[i] < unique[i + 1]) {
+            console.log(`Valleys: ${count}`);
         }
+        count++;
     }
-
-    console.log(count);
 }
 
 hillsValleys([2, 4, 1, 1, 6, 5])
